c744ed5f313f10faf0efe9dfbbf8b844ba643137
Khoa Le <kl58277@uga.edu>
2023-04-12 16:12:11 -0400

    major changes to main page

--- src/App.js
+++ src/App.js
@@ -7,31 +7,30 @@ function App() {
   return (
     <div className="App">
       <div className="header">
-        <img
-          src="https://cdn3.iconfinder.com/data/icons/valentine2/100/Valentine_love-11-512.png"
-          alt="wine glass"
-          id="app-logo"
-        />
-        <div className="titleName">
-          <h1>PHO</h1>
-          <h3>WithYou</h3>
+        <div className="logoName">
+          <img
+            src="https://cdn3.iconfinder.com/data/icons/valentine2/100/Valentine_love-11-512.png"
+            alt="wine glass"
+            id="app-logo"
+          />
+          <div className="titleName">
+            <h1>PHO</h1>
+            <h3>WithYou</h3>
+          </div>
         </div>
-       
-        <div>
+        <div className="buttons">
           <a href="https://react.school" target="_blank">
-            <Button buttonName="Log In"/>
+            <Button buttonName="Log In" />
           </a>
-      
           <Button buttonName="Sign Up" />
         </div>
       </div>
       <div className="body">
         <h1>"The best dating app ever" - Dylan Pettigrew</h1>
-        <br />
         <h2>Select your match</h2>
-        <Card>hello</Card>
+        <Card></Card>
         <h2>and connect!</h2>
-        <Card>hello</Card>
+        <Card></Card>
       </div>
       <Footer />
     </div>
