250d2fdfc03bfa99c44f4cb19d2e3fb8e73e3abc
ragegoney2 <gagemroney@gmail.com>
2023-04-13 15:22:54 -0400

    Added login/sign up pages and added some css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,46 @@
 import React from "react";
+import { useState } from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 export function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <form className="formLogin">
-     
-    </form>
+      <div className="signUpButton">
+        <p>Don't have an account?</p>
+        <Link to="/signup">
+          <Button buttonName="Sign Up" />
+        </Link>
+      </div>
+      <div className="formHeader">
+        <h1>Login</h1>
+      </div>
+        <div>
+          <label>Email:
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} />
+          </label>
+        </div>
+        <div>
+          <label>Password:
+            <input
+              type="text"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+          </label>
+        </div>
+
+        <div>
+        <Link to="/">
+          <Button buttonName="Sign In" />
+        </Link>
+        </div>
+      </form>
   );
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,3 +1,47 @@
 import React from "react";
+import { useState } from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import "./SignUp.css";
 
-export function SignUp(props) {}
+export function SignUp(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <form className="formSignUp">
+      <div className="signInButton">
+        <p>Already have an account?</p>
+        <p>Click here to sign in</p>
+        <Link to="/login">
+          <Button buttonName="Sign In" />
+        </Link>
+      </div>
+      <div className="formHeaderSignUp">
+        <h1>Sign Up</h1>
+      </div>
+        <div>
+          <label>Email:
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} />
+          </label>
+        </div>
+        <div>
+          <label>Password:
+            <input
+              type="text"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+          </label>
+        </div>
+
+        <div>
+        <Link to="/">
+          <Button buttonName="Sign Up" />
+        </Link>
+        </div>
+      </form>
+  );
+}
