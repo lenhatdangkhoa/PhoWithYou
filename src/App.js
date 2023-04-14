8e8931e023bc8bafa34429b7f305a32ce7ed4a75
ragegoney2 <gagemroney@gmail.com>
2023-04-14 15:58:32 -0400

    added edit profile page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Home } from "./components/Home";
 import { SignUp } from "./components/SignUp";
 import { Login } from "./components/Login";
 import { MainPage } from "./components/MainPage";
+import { EditProfile } from "./components/EditProfile";
 import { Route, Routes, Link } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/main-page" element={<MainPage />}></Route>
+      <Route path="/edit-profile" element={<EditProfile />}></Route>
     </Routes>
   );
 }
