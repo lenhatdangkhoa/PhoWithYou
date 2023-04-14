85d9b9a00f202bebaf333d2673e9f0c194fb314d
ragegoney2 <gagemroney@gmail.com>
2023-04-14 13:02:09 -0400

    added a little css to login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,7 @@ import "./Login.css";
 export function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const isLoggedIn = false;
 
   return (
     <form className="formLogin">
@@ -24,7 +25,9 @@ export function Login(props) {
             <input
               type="text"
               value={email}
-              onChange={(e) => setEmail(e.target.value)} />
+              onChange={(e) => setEmail(e.target.value)} 
+              className="emailBox"
+              />
           </label>
         </div>
         <div>
@@ -32,7 +35,9 @@ export function Login(props) {
             <input
               type="text"
               value={password}
-              onChange={(e) => setPassword(e.target.value)} />
+              onChange={(e) => setPassword(e.target.value)} 
+              className="passwordBox"
+              />
           </label>
         </div>
 
