df1fd00865afe983e4597c49bce5260d07d911a9
Nnamdi Obichi <2nnamdiobichi@gmail.com>
2023-04-19 14:08:58 -0400

    Formatting on landing page, login, and signup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -9,44 +9,52 @@ export function SignUp(props) {
   const [password, setPassword] = useState("");
 
   return (
-    <form className="formSignUp">
-      <div className="HomeButton">
-        <Link to="/">
-          <Button buttonName="Back to Landing..." />
-        </Link>
-      </div>
-      <div className="signInButton">
-        <p>Already have an account?</p>
-        <p>Click here to sign in</p>
-        <Link to="/login">
-          <Button buttonName="Sign In" />
-        </Link>
-      </div>
-      <div className="formHeaderSignUp">
-        <h1>Sign Up</h1>
-      </div>
-        <div>
-          <label>Email:
-            <input
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)} />
-          </label>
+    <div className="sgn">
+      <form className="formSignUp">
+        <div className="HomeButton">
+          <Link to="/">
+            <Button buttonName="Back to Landing..." />
+          </Link>
         </div>
-        <div>
-          <label>Password:
-            <input
-              type="text"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)} />
-          </label>
+        <div className="signInButton">
+          <p>Already have an account?</p>
+          <p>Click here to sign in</p>
+          <Link to="/login">
+            <Button buttonName="Sign In" />
+          </Link>
         </div>
+        <div className="formHeaderSignUp">
+          <h1>Sign Up</h1>
+        </div>
+          <div>
+            <label>
+              <input
+                type="text"
+                placeholder="Enter your email..."              
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className= "emailBox"
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              <input
+                type="text"
+                placeholder="Create a password..."   
+                value={password}
+                onChange={(e) => setPassword(e.target.value)} 
+                className= "passwordBox"
+              />
+            </label>
+          </div>
 
-        <div>
-        <Link to="/">
-          <Button buttonName="Sign Up" />
-        </Link>
+        <div className="signup">
+          <Link to="/">
+            <Button buttonName="Sign Up" />
+          </Link>
         </div>
       </form>
+    </div>
   );
 }
