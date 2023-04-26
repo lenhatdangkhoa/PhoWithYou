80d294a154ed0b17035ce00d34c5e76f38a63415
Khoa Le <kl58277@uga.edu>
2023-04-26 14:32:01 -0400

    merging

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const dotenv = require("dotenv");
-dotenv.config();
+dotenv.config({ path: "./phowithyou/.env" });
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const express = require("express");
